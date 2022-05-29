f8e307a375a5e35699a7eb55e47a422eb00a320a
Nathan Minkhorst <nathanminkhorst@gmail.com>
2022-05-29 19:41:42 +0200

    responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Logo from '../Assets/CineData Logo.png'
 import SearchBar from './SearchBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 import { AppContext } from '../App';
 
 const Nav = () => {
     const { setSearch } = useContext(AppContext)
     const mobileMenu = document.querySelector('.btn__menu')
+    const [menuOpen, setMenuOpen] = useState(false)
 
     function toggleMenu() {
         if(!document.body.classList.contains('menu-open')) {
             document.body.classList += ' menu-open'
-            mobileMenu.innerHTML = `<i class="fas fa-times"></i>`
+            setMenuOpen(true)
         } else {
             document.body.classList.remove('menu-open')
-            mobileMenu.innerHTML = `<i class="fas fa-bars"></i>`
+            setMenuOpen(false)
         }
     }
 
@@ -30,19 +31,19 @@ const Nav = () => {
                     <li><a className="nav__link--primary no-cursor">Contact</a></li>
                 </ul>
                 <button className="btn__menu" onClick={toggleMenu}>
-                    <FontAwesomeIcon icon={faBars} />
+                    <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} className='fa-bars'/>
                 </button>
             </div>
             <div className="menu__backdrop">
                 <ul className="mobile__links">
                     <li className="mobile__link">
-                        <a href="./index.html" className="mobile__link--anchor" onClick={toggleMenu}>Home</a>
+                        <Link to="/" className="mobile__link--anchor" onClick={toggleMenu}>Home</Link>
                     </li>
                     <li className="mobile__link">
-                        <a href="#" className="mobile__link--anchor no-cursor" onClick={toggleMenu}>Discover Movies</a>
+                        <Link to="/search" className="mobile__link--anchor" onClick={toggleMenu}>Discover Movies</Link>
                     </li>
                     <li className="mobile__link">
-                        <a href="#" className="mobile__link--anchor no-cursor" onClick={toggleMenu}>Contact</a>
+                        <a className="mobile__link--anchor no-cursor" onClick={toggleMenu}>Contact</a>
                     </li>
                 </ul>                        
             </div>
